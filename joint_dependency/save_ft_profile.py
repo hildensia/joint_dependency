92829be9231f37504ae8c653c50b6b7c85e1ba79
Stefan Otte <stefan.otte@gmail.com>
2014-09-29 14:18:36 +0200

    fix save_ft_profile:

--- joint_dependency/save_ft_profile.py
+++ joint_dependency/save_ft_profile.py
@@ -1,3 +1,8 @@
+"""
+Little helper script to record and pickle F/T profiles of the joints.
+The F/T profiles are used later to infer the JDS.
+"""
+
 from joint_dependency.ros_adapter import (create_ros_drawer_world,
                                           RosActionMachine)
 from joint_dependency.simulation import (create_world, ActionMachine,
@@ -11,33 +16,32 @@ import argparse
 
 __author__ = 'johannes'
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--with-ros", required=True,
-                        help="Should we use the real robot and ROS?",
-                        type=bool)
+    parser.add_argument('--useRos', action='store_true',
+                        help="Enable ROS/real robot usage.")
     args = parser.parse_args()
 
-    if args['with-ros']:
+    if args.useRos:
         world = create_ros_drawer_world()
         action_machine = RosActionMachine(world)
-        rospy.init_node("ft_reader")
     else:
         world = create_world()
         controller = [Controller(world, i)
                       for i, joint in enumerate(world.joints)]
         action_machine = ActionMachine(world, controller)
 
-
     jpos = np.array([int(j.get_q()) for j in world.joints])
 
-    for joint in world.joints:
+    for i, joint in enumerate(world.joints):
         action_pos = np.array(jpos)
-        action_pos[joint] = world.joints[joint].max_limit
+        action_pos[i] = world.joints[i].max_limit
         action_machine.run_action(action_pos)
-        action_pos[joint] = world.joints[joint].min_limit
+        action_pos[i] = world.joints[i].min_limit
         action_machine.run_action(action_pos)
 
+    # The actual data we want to pickle
     P_cp = update_p_cp(world)
     P_same = compute_p_same(P_cp)
 
@@ -45,5 +49,3 @@ if __name__ == '__main__':
 
     with open("cp_profile_{}.pkl".format(date), "w") as _file:
         cPickle.dump((P_cp, P_same), _file)
-
-
