53ca156518e76d48e6e41c6876594fd2efa6a418
Johannes Kulick <kulick@hildensia.de>
2014-09-29 14:51:12 +0200

    move recorder to own file

--- joint_dependency/simulation.py
+++ joint_dependency/simulation.py
@@ -1,40 +1,7 @@
 import numpy as np
 import pandas as pd
 import random
-
-
-class Record(object):
-    # record = pd.DataFrame()
-    records = {}
-
-    def __init__(self, columns):
-        self.columns = columns
-
-    def __call__(self, f):
-
-        def wrapped_f(*args, **kwargs):
-            data = f(*args, **kwargs)
-
-            if data is None:
-                return data
-
-            _self = args[0]
-            columns = list(self.columns)
-
-            if hasattr(_self, "index"):
-                for i, _ in enumerate(self.columns):
-                    columns[i] += "_" + str(_self.index)
-
-            rec = pd.DataFrame([data], index=_self.world.get_index(),
-                               columns=columns)
-
-            pid = multiprocessing.current_process().pid
-            old_record = Record.records.get(pid, pd.DataFrame())
-            Record.records[pid] = old_record.combine_first(rec)
-
-            return data
-
-        return wrapped_f
+from joint_dependency.recorder import Record
 
 
 def get_state(q, states):
