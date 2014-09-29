1f5f4647e15362c1e1224d67c4dc7e8ea65b22bf
Johannes Kulick <kulick@hildensia.de>
2014-09-29 15:09:53 +0200

    Add test for drawer with key

--- joint_dependency/tests/test_simulation.py
+++ joint_dependency/tests/test_simulation.py
@@ -1,7 +1,8 @@
 import unittest
 import numpy as np
 
-from joint_dependency.simulation import World, Joint, MultiLocker
+from joint_dependency.simulation import (World, Joint, MultiLocker,
+                                         create_drawer_with_key)
 
 
 class TestMultiLocker(unittest.TestCase):
@@ -89,3 +90,40 @@ class TestJoint(unittest.TestCase):
 
         self.world.joints[0].unlock()
         self.assertFalse(self.world.joints[0].is_locked())
+
+
+class TestFurniture(unittest.TestCase):
+    def setUp(self):
+        self.world = World([])
+
+
+    def test_drawer_with_key(self):
+        drawer_min = np.random.randint(0, 180)
+        drawer_max = np.random.randint(drawer_min, 180)
+        handle_min = np.random.randint(0, 180)
+        handle_max = np.random.randint(handle_min, 180)
+
+        create_drawer_with_key(self.world, 10e-3, [(handle_min, handle_max),
+                                                   (drawer_min, drawer_max)])
+
+        self.assertEqual(len(self.world.joints), 2)
+        self.assertEqual(self.world.joints[0].min_limit, handle_min)
+        self.assertEqual(self.world.joints[0].max_limit, handle_max)
+        self.assertEqual(self.world.joints[1].min_limit, drawer_min)
+        self.assertEqual(self.world.joints[1].max_limit, drawer_max)
+
+        for listener in self.world.listeners:
+            if isinstance(listener, MultiLocker):
+                self.assertEqual(listener.locks[0][0], handle_min)
+                self.assertEqual(listener.locks[1][1], handle_max)
+                self.assertLessEqual(listener.locks[1][0]-listener.locks[0][1],
+                                     20)
+                self.assertGreaterEqual(listener.locks[1][0]-listener.locks[0][1],
+                                        10)
+
+
+
+
+
+
+
