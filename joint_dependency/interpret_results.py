a34c8cc19802564b9496ffa5c7a46fe2d812bce4
top-1b <top-1b@robotics.tu-berlin.de>
2016-07-21 11:22:22 +0200

    added functionality for plotting entropy

--- joint_dependency/interpret_results.py
+++ joint_dependency/interpret_results.py
@@ -5,12 +5,15 @@ Created on Thu Jul 21 10:23:46 2016
 
 import argparse
 import cPickle
+import seaborn as sns
 
 import matplotlib.pylab as plt
 import pandas as pd
 import numpy as np
 
 from matplotlib.lines import Line2D
+
+sns.set_style("darkgrid")
 
 def plot_locking_states(df, meta):
 
@@ -44,6 +47,31 @@ def plot_locking_states(df, meta):
     
     plt.plot()
 
+def plot_entropy(df, meta):
+    plt.figure()    
+    for j in range(5):
+        var_name="Entropy%d"%j
+        plt.plot(df[var_name], label=var_name)
+    plt.legend()
+    
+
+def plot_dependency_posterior(df, meta, t):
+    plt.figure()
+    posterior=np.array([df["Posterior%d"%j].iloc(t)[:] for j in range(5)])
+    print posterior
+    print posterior.shape    
+    plt.matshow(posterior, interpolation='nearest')
+    plt.show()
+    
+#Index([u'DesiredPos0', u'DesiredPos1', u'DesiredPos2', u'DesiredPos3',
+#       u'DesiredPos4', u'CheckedJoint', u'RealPos0', u'RealPos1', u'RealPos2',
+#       u'RealPos3', u'RealPos4', u'LockingState0', u'LockingState1',
+#       u'LockingState2', u'LockingState3', u'LockingState4', u'Posterior0',
+#       u'Entropy0', u'Posterior1', u'Entropy1', u'Posterior2', u'Entropy2',
+#       u'Posterior3', u'Entropy3', u'Posterior4', u'Entropy4'],
+#      dtype='object')
+
+
 def open_pickle_file(pkl_file):
     with open(pkl_file) as f:
         df, meta = cPickle.load(f)
@@ -55,11 +83,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", "--file", required=True,
                         help="pickle file")
-
     args = parser.parse_args()  
     
     df, meta = open_pickle_file(args.file)
     
-    plot_locking_states(df, meta)
-    
+    #plot_locking_states(df, meta)
+    plot_entropy(df,meta)
+    plot_dependency_posterior(df,meta,-1)
     plt.show()
