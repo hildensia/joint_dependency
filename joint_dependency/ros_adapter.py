552da1d7910cfac6e35658d8e6c39fb2081ca0c8
Johannes Kulick <kulick@hildensia.de>
2016-09-07 15:01:25 +0200

    fixed many issues with the ros adapter

--- joint_dependency/ros_adapter.py
+++ joint_dependency/ros_adapter.py
@@ -5,7 +5,8 @@ from collections import defaultdict
 
 try:
     import roslib
-    roslib.load_manifest('joint_dependency')
+    roslib.load_manifest('joint_dependency_ros')
+    from std_msgs.msg import Int32, Bool, Float32
     import rospy
 except ImportError:
     print("Disable ROS.")
@@ -18,15 +19,16 @@ def create_ros_lockbox():
     rospy.init_node("joint_dependency")
     joints = []
     for i in range(5):
-        joints.append(RosJoint("lock_{}".format(i), [0, 180]))
+        joints.append(RosJoint("lock_{}".format(i), i+1, [0, 180]))
+    joints[0].locked = False
     world = RosWorld(joints)
     return world
 
 
 class FakeService(object):
-    def __init__(self, request, response):
-        self.publisher = rospy.Publisher(request)
-        self.subscriber = rospy.Subscriber(response, self.callback)
+    def __init__(self, request, response, request_t, response_t):
+        self.publisher = rospy.Publisher(request, request_t)
+        self.subscriber = rospy.Subscriber(response, response_t, self.callback)
         self.semaphore = threading.Semaphore(0)
         self.response = None
 
@@ -41,16 +43,22 @@ class FakeService(object):
 
 
 class RosJoint(object):
-    listener = None
-
-    def __init__(self, name, limits):
+    def __init__(self, name, index, limits):
         self.name = name
+        self.index = index
         self.min_limit = limits[0]
         self.max_limit = limits[1]
-        self.get_q_srv = FakeService('get_q', 'get_q_response')
+        self.locked = True
+        self.get_q_srv = FakeService('get_q', 'get_q_response', Int32, Int32)
 
     def get_q(self, time=None):
-        return self.get_q_srv(self.name)
+        print("get_q {}".format(self.index))
+        q = int(self.get_q_srv(self.index).data)
+        print(q)
+        return q
+
+    def is_locked(self):
+        return self.locked
 
 
 class RosWorld(object):
@@ -62,11 +70,15 @@ class RosActionMachine(object):
     def __init__(self, world):
         self.world = world
         self.locking_state = defaultdict(bool)  # returns False on empty bucket
-        self.run_action_srv = FakeService('run_action', 'run_action_done')
+        self.run_action_srv = FakeService('run_action', 'run_action_done',
+                Int32, Bool)
 
-    def run_action(self, pos, joint):
-        self.locking_state[joint] = self.run_action_srv(joint)
+    def run_action(self, pos, joint_idx):
+        joint = self.world.joints[joint_idx]
+        print("run_action: {}".format(joint))
+        joint.locked = not bool(self.run_action_srv(joint.index).data)
+        print(joint.is_locked())
 
-    def check_state(self, joint):
-        return self.locking_state[joint]
+    def check_state(self, joint_idx):
+        return self.world.joints[joint_idx].is_locked()
 
