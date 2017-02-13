326fd2c76a2c2ddd6bb27dee4a957f147e1d3a4d
Roberto Martin-Martin <roberto.martinmartin@tu-berlin.de>
2017-02-13 14:58:04 +0100

    new yamls and script for scritps

---
+++ scripts/create_plots.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+ 
+python ../joint_dependency/interpret_results.py -f data_2017-02-10-16-35-24.553420_random_objective.pkl
+python ../joint_dependency/interpret_results.py -f data_2017-02-10-16-37-55.720714_exp_neg_entropy.pkl
+python ../joint_dependency/interpret_results.py -f data_2017-02-10-16-39-34.086957_exp_cross_entropy.pkl
+python ../joint_dependency/interpret_results.py -f data_2017-02-10-16-41-48.249906_heuristic_proximity.pkl
+python ../joint_dependency/interpret_results.py -f data_2017-02-10-16-44-26.078087_random_objective.pkl
+python ../joint_dependency/interpret_results.py -f data_2017-02-10-16-46-40.050877_exp_neg_entropy.pkl
+python ../joint_dependency/interpret_results.py -f data_2017-02-10-16-49-01.331408_exp_cross_entropy.pkl
+python ../joint_dependency/interpret_results.py -f data_2017-02-10-16-51-25.386335_heuristic_proximity.pkl
+python ../joint_dependency/interpret_results.py -f data_2017-02-10-16-53-36.314827_random_objective.pkl
+python ../joint_dependency/interpret_results.py -f data_2017-02-10-16-55-58.194054_exp_neg_entropy.pkl
+python ../joint_dependency/interpret_results.py -f data_2017-02-10-16-57-54.691622_exp_cross_entropy.pkl
+python ../joint_dependency/interpret_results.py -f data_2017-02-10-16-59-16.787580_heuristic_proximity.pkl
+python ../joint_dependency/interpret_results.py -f data_2017-02-10-17-01-07.922953_random_objective.pkl
+python ../joint_dependency/interpret_results.py -f data_2017-02-10-17-06-02.863293_exp_neg_entropy.pkl
+python ../joint_dependency/interpret_results.py -f data_2017-02-10-17-07-49.720599_exp_cross_entropy.pkl
+python ../joint_dependency/interpret_results.py -f data_2017-02-10-17-10-18.020004_heuristic_proximity.pkl
+python ../joint_dependency/interpret_results.py -f data_2017-02-10-17-18-06.216588_random_objective.pkl
+python ../joint_dependency/interpret_results.py -f data_2017-02-10-17-21-13.898009_exp_neg_entropy.pkl
+python ../joint_dependency/interpret_results.py -f data_2017-02-10-17-23-12.968836_exp_cross_entropy.pkl
+python ../joint_dependency/interpret_results.py -f data_2017-02-10-17-24-42.184778_heuristic_proximity.pkl
+python ../joint_dependency/interpret_results.py -f data_2017-02-10-17-27-28.546884_random_objective.pkl
+python ../joint_dependency/interpret_results.py -f data_2017-02-10-17-29-51.772107_exp_neg_entropy.pkl
+python ../joint_dependency/interpret_results.py -f data_2017-02-10-17-34-37.521528_exp_cross_entropy.pkl
+python ../joint_dependency/interpret_results.py -f data_2017-02-10-17-39-06.236831_heuristic_proximity.pkl
